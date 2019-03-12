fa1cfac24a696a940bc582964d1d9b8ee9219fd5
J2ghz <j2.00ghz@gmail.com>
2019-03-12 14:09:57 +0100

    Generate project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBD___Compulsory_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
