17b5af952e9e1684ccf7141860ba66d1ea3fc6c1
Mikkel1996DK <Mikkelljungberg@gmail.com>
2019-03-12 15:22:20 +0100

    Added exceptions to the stored procedure

--- Initialize.sql
+++ Initialize.sql
@@ -1,8 +1,16 @@
-﻿CREATE PROCEDURE dbo.usp_CreateDepartment 
+﻿ALTER PROCEDURE dbo.usp_CreateDepartment 
     @DName NVARCHAR (50),
     @MgrSSN NUMERIC (9)  
 AS
     BEGIN
+		
+		IF EXISTS (SELECT 1 FROM Department WHERE DName = @DName)
+			THROW 50001, 'Depmartment name already exists!', 1;
+
+		IF EXISTS (SELECT 1 FROM Department WHERE MgrSSN = @MgrSSN)
+			THROW 50002, 'Department manager already exists!', 1;
+		
+
 		DECLARE @DepNumber AS INTEGER;
 
 		SET @DepNumber = (SELECT MAX(DNumber) + 1 FROM Department)
@@ -12,6 +20,6 @@ AS
 RETURN @DepNumber
 
 GO
-EXEC dbo.usp_CreateDepartment "a", 123456789
+EXEC dbo.usp_CreateDepartment 'Administration', 123456789
 GO
-SELECT * FROM Department WHERE DName = "a"
+SELECT * FROM Department WHERE DName = 'Administration'
