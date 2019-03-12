e5ac61b77cfe542e4bf61e7bfa7c1cca5c717a80
Mikkel1996DK <Mikkelljungberg@gmail.com>
2019-03-12 15:06:07 +0100

    Copied over working SQL code

--- Initialize.sql
+++ Initialize.sql
@@ -4,33 +4,14 @@
 AS
     BEGIN
 		DECLARE @DepNumber AS INTEGER;
-		/*
-		DECLARE @HighestDepNumber TABLE (DName NVARCHAR(50) NULL,
-										 DNumber INTEGER NOT NULL,
-										 MgrSSN NUMERIC(9, 0) NULL,
-										 MgrStartDate DATETIME NULL)
-		
-		*/
 
-		SELECT @DepNumber = [DNumber]
-		FROM Department
-		
-
-
-
-		SELECT MAX(DNumber) INTO #DepNumber FROM Department
-		--SELECT MAX(DNumber) +1 INTO @DepNumber FROM Department
-
-		INSERT INTO Department (DName, DNumber, MgrSSN, MgrStartDate)
-		SELECT 
-
-
-		INSERT INTO Department(DNumber, DName, MgrSSN)
-		VALUES (@DepNumber, @DName, @MgrSSN)
+		SET @DepNumber = (SELECT MAX(DNumber) + 1 FROM Department)
+		INSERT INTO Department(DNumber, DName, MgrSSN, MgrStartDate)
+		VALUES (@DepNumber, @DName, @MgrSSN, GETDATE())
 	END
 RETURN @DepNumber
 
 GO
-EXEC dbo.usp_CreateDepartment "test", 123456789
+EXEC dbo.usp_CreateDepartment "a", 123456789
 GO
-SELECT * FROM Department WHERE DName = "test"
+SELECT * FROM Department WHERE DName = "a"
