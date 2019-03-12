12f7f061258d31a1f5b839aa94a50931e69a3163
MartynasTumas <martynas.tumas@gmail.com>
2019-03-12 16:37:20 +0100

    dbo.usp_DeleteDepartment store procedure created

--- Initialize.sql
+++ Initialize.sql
@@ -23,3 +23,31 @@ GO
 EXEC dbo.usp_CreateDepartment 'Administration', 123456789
 GO
 SELECT * FROM Department WHERE DName = 'Administration'
+
+
+
+GO
+ALTER PROCEDURE dbo.usp_DeleteDepartment(@DNumberDelete INT)
+AS
+
+		IF EXISTS (SELECT 1 FROM Department WHERE DNumber = @DNumberDelete)		
+		BEGIN
+
+			--Declare table with all project numbers from the departrment that will be deleted
+			DECLARE @ProjectNoTbl TABLE (ProjectNo INTEGER);	
+			INSERT INTO @ProjectNoTbl (ProjectNo) (SELECT PNumber FROM Project WHERE DNum = @DNumberDelete)
+
+			--Delete everything ralated to the department
+			DELETE FROM Works_on WHERE Pno in (SELECT ProjectNo FROM @ProjectNoTbl);
+			DELETE FROM Project WHERE DNum = @DNumberDelete;
+			DELETE FROM Dept_Locations WHERE DNUmber = @DNumberDelete;
+			DELETE FROM Department WHERE DNumber = @DNumberDelete;
+			
+			UPDATE Employee SET Dno = NULL WHERE Dno = @DNumberDelete;
+			
+			END
+
+RETURN;
+
+GO
+EXEC dbo.usp_DeleteDepartment 4
