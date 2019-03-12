be5ec490fdd3b0b6a21158d1438bcd87f59d55dd
J2ghz <j2.00ghz@gmail.com>
2019-03-12 15:57:41 +0100

    sql client

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DBD___Compulsory_Assignment_1
 {
@@ -6,7 +8,23 @@ namespace DBD___Compulsory_Assignment_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var sqlConnection = new SqlConnection(@"Server=mssql.jacobhinze.dk;Database=DB_SearchEngine;Enlist=False;User ID=mikkel;Password=eerrddff11,,;"))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = new SqlCommand("EXECUTE dbo.usp_GetAllDepartments", sqlConnection))
+                {
+                    var reader = sqlCommand.ExecuteReader();
+                    var schemaTable = reader.GetSchemaTable();
+
+                    foreach (DataRow row in schemaTable.Rows)
+                    {
+                        foreach (DataColumn column in schemaTable.Columns)
+                        {
+                            Console.WriteLine($"{column.ColumnName} = {row[column]}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
